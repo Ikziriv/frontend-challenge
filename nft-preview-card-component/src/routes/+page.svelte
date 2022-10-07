180a8cbc44cc0e9158a47acf8c0bc01c532bb0aa
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-10-08 01:38:40 +0700

    Fix Active States

--- nft-preview-card-component/src/routes/+page.svelte
+++ nft-preview-card-component/src/routes/+page.svelte
@@ -1,13 +1,45 @@
+<script>
+	import { fade } from 'svelte/transition';
+    import Icon from '@iconify/svelte';
+	
+	let hover = false;
+    let navigating = false;
+</script>
+
 <div class="flex justify-center items-center w-full h-screen md:h-screen bg-bgm-dark-blue p-4 md:p-0">
     <div class="w-80 md:w-80 h-auto md:max-w-md bg-bgc-dark-blue rounded-2xl p-5 md:p-5 shadow-md">
         <div class="grid grid-cols-1 gap-0 w-full h-auto">
             <div class="col-span-full w-full h-64 md:h-auto rounded-2xl pb-4">
-                <img class="rounded-xl w-full h-full object-cover object-center" src="/image-equilibrium.jpg" alt="Equilibrium" />
+                <div class="justify-center items-center w-full h-full relative"
+                    on:mouseenter={() => {
+                        console.log("enter", navigating);
+                        if (!navigating) {
+                            hover = true;
+                        }
+                    }}
+                    on:mouseleave={() => {
+                        console.log("leave", navigating);
+                        if (!navigating) {
+                            hover = false;
+                        }
+                    }}>
+
+                    {#if hover}
+                    <div transition:fade class="absolute inset-0 w-full h-full rounded-xl">
+                        <div class="flex justify-center items-center w-full h-full bg-cyan/50 rounded-xl">
+                            <span class="w-auto h-auto">
+                                <Icon class="w-6 md:w-8 h-6 md:h-8 text-white" icon="ion:eye" />
+                            </span>
+                        </div>
+                    </div>
+                    {/if}
+                    <img class="rounded-xl w-full h-full object-cover object-center z-0" src="/image-equilibrium.jpg" alt="Equilibrium" />
+                </div>
             </div>
             <div class="col-span-full w-full h-auto">
                 <div class="flex flex-col space-y-2 justify-center items-start w-full h-auto p-2 bg-transparent">
                     <div class="flex flex-col space-y-3 w-auto h-auto">
-                        <h1 class="text-xl font-outfit-black mb-2 text-white text-left">
+                        <h1 class="text-xl font-outfit-black mb-2 text-white hover:text-cyan text-left">
                             Equilibrium #3429 
                         </h1>
                     </div>
@@ -32,7 +64,7 @@
                         <img class="w-8 h-8" src="/image-avatar.png" alt="AvatarImage">
                         <div class="flex space-x-1 items-center">
                             <span class="text-sm text-soft-blue">Creation of</span>
-                            <span class="text-sm text-white">Jules Wyvern</span>
+                            <span class="text-sm text-white hover:text-cyan">Jules Wyvern</span>
                         </div>
                     </div>
                 </div>
--- nft-preview-card-component/tailwind.config.cjs
+++ nft-preview-card-component/tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
           colors: {
               'soft-blue': 'hsl(215, 51%, 70%)',
               'cyan': 'hsl(178, 100%, 50%)',
+              'bg-cyan': 'hsl(178, 100%, 50%)',
               'bgm-dark-blue': 'hsl(217, 54%, 11%)',
               'bgc-dark-blue': 'hsl(216, 50%, 16%)',
               'bgl-dark-blue': 'hsl(215, 32%, 27%)',
